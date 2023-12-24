60bb91328c9c5dd0e6b4322ab5edffff0fe2ab0a
Levent Keles <levent@webtakibi.com>
2023-12-24 06:50:10 +0300

    anasayfanın en üstüne thumbnail eklendi

--- lib/view/main/subviews/home/home_view.dart
+++ lib/view/main/subviews/home/home_view.dart
@@ -8,62 +8,53 @@ part 'home_view.g.dart';
 
 @swidget
 Widget homeView(void data) => ViewModelBuilder<HomeViewmodel>.reactive(
-      viewModelBuilder: () => HomeViewmodel(),
-      builder: (_, model, __) => Scaffold(
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 60,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: model.categories.length,
-                  itemBuilder: (_, index) {
-                    final category = model.categories[index];
-                    return GestureDetector(
-                      onTap: () => model.filterProductsByCategory(category),
-                      child: Container(
-                        margin: EdgeInsets.all(8),
-                        color: Colors.grey[200],
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: DDimens.s, horizontal: DDimens.l),
-                            child:
-                                Text(category, style: TextStyle(fontSize: 16)),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              // Ürün Listesi
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.8,
-                  ),
-                  itemCount: model.displayedProducts.length,
-                  itemBuilder: (_, index) {
-                    final product = model.displayedProducts[index];
-                    return productWidget(product,
-                        addToCart: model.addToCart,
-                        addToFavorite: (product) {});
-                  },
-                ),
-              ),
-            ],
+  viewModelBuilder: () => HomeViewmodel(),
+  builder: (_, model, __) => Scaffold(
+    body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0), // İstediğiniz padding değerlerini ayarlayın
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(6.0), // Yuvarlaklık derecesini ayarlayın
+              child: Image.asset('lib/resources/pawketthumb.png'),
+            ),
           ),
-        ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: () => model.showCart(),
-        //   child: Icon(Icons.shopping_cart),
-        // ),
+
+          Container(
+            height: 60,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: model.categories.length,
+              itemBuilder: (_, index) {
+                final category = model.categories[index];
+                return GestureDetector(
+                  onTap: () => model.filterProductsByCategory(category),
+                  child: Container(
+                    margin: EdgeInsets.all(8),
+                    color: Colors.grey[200],
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: DDimens.s, horizontal: DDimens.l),
+                        child:
+                        Text(category, style: TextStyle(fontSize: 16)),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          // ...
+        ],
       ),
-    );
+    ),
+    // ...
+  ),
+);
+
 
 class Product {
   final String name;
@@ -74,3 +65,6 @@ class Product {
 
   Product(this.name, this.price, this.imageUrl, this.category, this.isFavorite);
 }
+
+
+
