85bbe594ad4376b8944b17790587e8c62ef33239
Mehmet Arsay <mehmetafif.arsay@marti.tech>
2023-12-24 03:17:27 +0300

    added bottom navigation bar

--- lib/view/main/main_view.dart
+++ lib/view/main/main_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
-import 'package:pawcat/resources/d_text_styles.dart';
+import 'package:pawcat/resources/d_colors.dart';
 import 'package:pawcat/view/main/main_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -10,18 +10,35 @@ part 'main_view.g.dart';
 Widget mainView(void data) => ViewModelBuilder<MainViewModel>.reactive(
       viewModelBuilder: () => MainViewModel(),
       builder: (_, model, __) => Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: () => model.changeUser('Murat'),
-            child: Column(
-              children: [
-                Text(
-                  model.user,
-                  style: DTextStyles.darkBold,
-                ),
-                
-              ],
+        extendBody: true,
+        body: IndexedStack(index: model.currentIndex, children: [for (var i in model.pages) i.page]),
+        bottomNavigationBar: Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(blurRadius: 10, color: Colors.grey.withOpacity(0.2), offset: Offset(1, 2))],
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25),
+              topLeft: Radius.circular(25),
             ),
+          ),
+          child: BottomNavigationBar(
+            selectedFontSize: 12.0,
+            unselectedFontSize: 12.0,
+            elevation: 1,
+            type: BottomNavigationBarType.fixed,
+            landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
+            backgroundColor: Colors.white,
+            currentIndex: model.currentIndex,
+            onTap: model.setIndex,
+            selectedItemColor: DColors.primary,
+            items: [
+              for (var i in model.pages)
+                BottomNavigationBarItem(
+                  label: i.label,
+                  icon: i.icon,
+                  activeIcon: i.activeIcon,
+                ),
+            ],
           ),
         ),
       ),
