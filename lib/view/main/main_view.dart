d536714a99415159e2d187a46cbd258cbf572bfd
Levent Keles <levent@webtakibi.com>
2023-12-24 05:36:35 +0300

    appbar eklendi, googlefonts kütüphanesi eklendi

--- lib/view/main/main_view.dart
+++ lib/view/main/main_view.dart
@@ -3,6 +3,7 @@ import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:pawcat/resources/d_colors.dart';
 import 'package:pawcat/view/main/main_view_model.dart';
 import 'package:stacked/stacked.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 part 'main_view.g.dart';
 
@@ -10,6 +11,13 @@ part 'main_view.g.dart';
 Widget mainView(void data) => ViewModelBuilder<MainViewModel>.reactive(
       viewModelBuilder: () => MainViewModel(),
       builder: (_, model, __) => Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 70,
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Color(0xff021f33),
+          title:  Text("pawket", style: GoogleFonts.fredoka(color: Colors.white, textStyle: const TextStyle(fontSize: 40, fontWeight: FontWeight.w500),), ),
+        ),
         extendBody: true,
         body: IndexedStack(index: model.currentIndex, children: [for (var i in model.pages) i.page]),
         bottomNavigationBar: Container(
